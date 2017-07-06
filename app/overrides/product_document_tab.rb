e19e6b8a54595a76b8e33074bc5928ebf8ab4934
stm9x9 <github.20.stm@spamgourmet.com>
2017-07-06 23:01:00 +0200

    Update product_document_tab.rb

--- app/overrides/product_document_tab.rb
+++ app/overrides/product_document_tab.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
                  :name => "admin_product_document_tab",
                  :original => '10edf2223cf4fe1ebcd351beb56472f5b623ea6b',
-                 :insert_after => "ul[data-hook=admin_product_tabs] > li:first",
+                 :insert_bottom => "[data-hook='admin_product_tabs']",
                  :text => "
                   <%= content_tag :li, :class => ('active' if current == 'Documents') do %>
                     <%= link_to_with_icon 'file', I18n.t('spree.admin.tab.documents'), admin_product_documents_path(@product) %>
