71098a6e9073fb3fe1fee11e9ac9b10fa2572a76
stm9x9 <github.20.stm@spamgourmet.com>
2017-07-16 02:32:26 +0200

    Update add_docs_to_product.rb

--- app/overrides/add_docs_to_product.rb
+++ app/overrides/add_docs_to_product.rb
@@ -7,7 +7,7 @@ Deface::Override.new(
 
 <% unless @product.documents.any? || @product.variant_documents.any? %>
 <% else %>
-  <h3 class="product-section-title"><%= Spree.t(:documents) %></h3>
+  <h3 id="documents-title" class="product-section-title"><%= Spree.t(:documents) %></h3>
   <table class="table table-striped"  style="font-size: small" data-hook="documents_table">
     <thead>
       <tr data-hook="documents_header">
